311a5e75b3b35f06f56d2963dc896735503a7959
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-02-21 15:57:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 export default function App() {
 
   const nome = "Victor Hugo";
-  
+  const neyma = "Neymar";
   return(
     <div>
         <h1>olá Neymar</h1>
         <p>Desenvolvido por: {nome}</p>
+        <p>ai ai Neymar </p>
     </div>  
   );
-
-
+  
+  
+ 
 }
