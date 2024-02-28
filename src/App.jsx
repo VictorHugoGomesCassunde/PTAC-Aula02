dc85d86ecfb5b62741c082885b3db8d3f08ba111
VictorHugoGomesCassunde <victorhugocassunde10@gmail.com>
2024-02-28 16:05:44 -0400

    ytrhthy

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import Haeder from "./components/Header";
+
 export default function App() {
 
-  const nome = "Victor Hugo";
-  const neyma = "Neymar";
   return(
-    <div>
-        <h1>olá Neymar</h1>
-        <p>Desenvolvido por: {nome}</p>
-        <p>ai ai Neymar </p>
-    </div>  
+  <main>
+    <Haeder/>
+    <Haeder/>
+    <Haeder/>
+    <Haeder/>
+  </main>
   );
   
   
---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Haeder(){
+    return(
+        <header>
+          <h1>Haeder</h1>
+        </header>
+    );
+}
