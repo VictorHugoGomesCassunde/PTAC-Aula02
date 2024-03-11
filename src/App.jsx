bc89f53c81656502445b592ae6594b1250c3d82e
VictorHugoGomesCassunde <159331195+VictorHugoGomesCassunde@users.noreply.github.com>
2024-03-11 09:33:14 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import Haeder from "./components/Header";
-
+import Footer from "./components/Footer";
+import "./globals.css";
 export default function App() {
 
   return(
   <main>
-    <Haeder/>
-    <Haeder/>
-    <Haeder/>
+   
+    <Footer/>
     <Haeder/>
   </main>
   );
